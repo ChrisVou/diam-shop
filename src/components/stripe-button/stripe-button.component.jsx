8d02ce1218d7f95ebb5f99c8b15f9ebeecc305ec
unknown <ChrisVou@hotmail.com>
2020-11-04 23:03:13 +0200

    updating stripe integration and updating our favicon and title

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  //I stripe px ta 50 euro thelei na ta vlepei 5000 opote kanoume tin timi * 100
+  const priceForStripe = price * 100;
+  const publishableKey = 'pk_test_51HjsSbKDX9SFFUh59bJiuXPml5I7L6BldhIf5gMFpFxQJDFIuYF6OLXC40FZQvGeI4rQKw5dYMfuwXryC982jIfz00m8VCeQ0p';
+
+const onToken = token => {
+  console.log(token);
+  alert('Payment Successful');
+}
+
+//Ta soixeia afta ta peirame apo to github
+  return (
+    <StripeCheckout 
+      label='Pay Now'
+      name='DIAM Shop'
+      billingAddress
+      shippingAddress
+      image='https://svgshare.com/i/RDD.svg'
+      description={`Your total is €€{price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
